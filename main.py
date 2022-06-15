da421bf6cd34880c8a15f0eda72762b09adaf9d7
Thiraphat thamprayot <thiraphat.th@ku.th>
2022-06-15 11:07:54 +0700

    แล้วๆ

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ async def series(r1,r2,r3):
     serial = (f'serial = {1/float(r1)+1/float(r2)+1/float(r3)**-1:.2f}')
     return  serial,parallel 
 
+print("tests")
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="192.168.90.190", port=8080)
